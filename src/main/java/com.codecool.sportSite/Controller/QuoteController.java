b7f31bf49a45e25ea14725484ed57cf36d1a5852
zoltanpataki <zolee7777@gmail.com>
2018-04-20 16:32:34 +0200

    quote api is ready to use locally

--- src/main/java/com.codecool.sportSite/Controller/QuoteController.java
+++ src/main/java/com.codecool.sportSite/Controller/QuoteController.java
@@ -1,4 +1,23 @@
 package com.codecool.sportSite.Controller;
 
+import com.codecool.sportSite.Service.QuoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
 public class QuoteController {
+
+    @Autowired
+    QuoteService quoteService;
+
+    @GetMapping(value = "/new-quote")
+    public ResponseEntity getNewQuote() throws IOException{
+        return new ResponseEntity<>(quoteService.makeUrl(), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com.codecool.sportSite/Service/QuoteService.java
+++ src/main/java/com.codecool.sportSite/Service/QuoteService.java
@@ -1,4 +1,22 @@
 package com.codecool.sportSite.Service;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class QuoteService {
+    private final String url = "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+    public Map<String, String> makeUrl() throws IOException{
+        System.setProperty("http.agent", "Chrome");
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(JsonParser.Feature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER, true);
+        Map<String, String> myMap = mapper.readValue(new URL(url), HashMap.class);
+        return myMap;
+    }
 }
